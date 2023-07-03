c7592c1f95de81f7eb94b9c5c0859cc2a9bcd0ef
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-03 14:19:04 +0000

    Лекция №2

---
+++ lec_2_break_continue.py
@@ -0,0 +1,9 @@
+for i in 'hello world':
+    if i == 'o':
+        break
+    print(i)
+
+for i in 'hello world':
+    if i == '0':
+        continue
+    print(i)
---
+++ lec_2_logical_operations.py
@@ -0,0 +1,21 @@
+a = 3
+b = 4
+c = 5
+
+if a > 4 and b == 2:
+    print('Good')
+elif b > 3 or c == 5:
+    print('Best')
+else:
+    print('Bad')
+
+a = 3
+b = 5
+c = -1
+
+if a >= 3 or b == 0:
+    print('Good')
+elif c == -1 or b > 4:
+    print('Best')
+else:
+    print('Bad')
