5a817728d79e01e8a27d7cc925d9b4176a5c5401
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-05 21:49:22 +0200

    Update lab_2_task_4.py

--- lab_2_task_4.py
+++ lab_2_task_4.py
@@ -1,21 +1,27 @@
-a = int(input('Введите число элементов'))
-b = 1
-c = [1]
-d = 1
-e = 2
+#a = int(input('Введите число элементов'))
+#b = 1
+#c = [1]
+#d = 1
+#e = 2
+#
+#if a < 1:
+#    print('Введя число, которое больше 0 дурак')
+#elif a == 1:
+#    print(c)
+#else:
+#    c.append(d)
+#while a - e > 0:
+#    c.append((c[d] + c[d-1]))
+#    d += 1
+#    e += 1
+#print(c)
 
-if a < 1:
-    print('Введя число, которое больше 0 дурак')
-elif a == 1:
-    print(c)
-else:
-    c.append(d)
-while a - e > 0:
-    c.append((c[d] + c[d-1]))
-    d += 1
-    e += 1
-print(c)
 
+a = int(input('Введите число элементов: '))
+b = [1, 1]
 
-
+for i in range(0, a - 2, 1):
+    c = b[i] + b[ i + 1]
+    b.append(c)
+print(b)
 
