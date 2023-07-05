c7df04020c9f563891ea5a8cc6c2ae4b044ce7c2
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-05 21:54:46 +0200

    Update lab_2_task_4.py

--- lab_2_task_4.py
+++ lab_2_task_4.py
@@ -6,8 +6,6 @@
 #
 #if a < 1:
 #    print('Введя число, которое больше 0 дурак')
-#elif a == 1:
-#    print(c)
 #else:
 #    c.append(d)
 #while a - e > 0:
@@ -23,5 +21,9 @@ b = [1, 1]
 for i in range(0, a - 2, 1):
     c = b[i] + b[ i + 1]
     b.append(c)
-print(b)
-
+if a == 0:
+    print('[]')
+elif a == 1:
+    print('[1]')
+else:
+    print(b)
