99a91cd8881085cea24c0ff310e96f8cabf734c5
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-05 14:17:14 +0200

    Update lab_2_task_6.py

--- lab_2_task_6.py
+++ lab_2_task_6.py
@@ -1,4 +1,152 @@
-e = 1
-while e < 10:
-    print(e, 2*e, 3*e, 4*e, 5*e, 6*e, 7*e, 8*e, 9*e)
-    e += 1
+a = []
+b = []
+c = []
+d = []
+e = []
+f = []
+g = []
+h = []
+j = []
+k = []
+l = 0
+
+a.append(1)
+a.append(2)
+a.append(3)
+a.append(4)
+a.append(5)
+a.append(6)
+a.append(7)
+a.append(8)
+a.append(9)
+a.append(10)
+a.append(11)
+a.append(12)
+a.append(13)
+a.append(14)
+a.append(15)
+a.append(16)
+a.append(17)
+a.append(18)
+a.append(19)
+a.append(20)
+a.append(21)
+a.append(22)
+a.append(23)
+a.append(24)
+a.append(25)
+a.append(26)
+a.append(27)
+a.append(28)
+a.append(29)
+a.append(30)
+a.append(31)
+a.append(32)
+a.append(33)
+a.append(34)
+a.append(35)
+a.append(36)
+a.append(37)
+a.append(38)
+a.append(39)
+a.append(40)
+a.append(41)
+a.append(42)
+a.append(43)
+a.append(44)
+a.append(45)
+a.append(46)
+a.append(47)
+a.append(48)
+a.append(49)
+a.append(50)
+a.append(51)
+a.append(52)
+a.append(53)
+a.append(54)
+a.append(55)
+a.append(56)
+a.append(57)
+a.append(58)
+a.append(59)
+a.append(60)
+a.append(61)
+a.append(62)
+a.append(63)
+a.append(64)
+a.append(65)
+a.append(66)
+a.append(67)
+a.append(68)
+a.append(69)
+a.append(70)
+a.append(71)
+a.append(72)
+a.append(73)
+a.append(74)
+a.append(75)
+a.append(76)
+a.append(77)
+a.append(78)
+a.append(79)
+a.append(80)
+a.append(81)
+for i in range(0, 81, 1):
+    if a[i] == 1 or a[i] == 2 or a[i] == 3 or a[i] == 4 or a[i] == 5 or a[i] == 6 or a[i] == 7 or a[i] == 8 or a[i] == 9:
+        b.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 2 or a[i] == 4 or a[i] == 6 or a[i] == 8 or a[i] == 10 or a[i] == 12 or a[i] == 14 or a[i] == 16 or a[i] == 18:
+        c.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 3 or a[i] == 6 or a[i] == 9 or a[i] == 12 or a[i] == 15 or a[i] == 18 or a[i] == 21 or a[i] == 24 or a[i] == 27:
+        d.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 4 or a[i] == 8 or a[i] == 12 or a[i] == 16 or a[i] == 20 or a[i] == 24 or a[i] == 28 or a[i] == 32 or a[i] == 36:
+        e.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 5 or a[i] == 10 or a[i] == 15 or a[i] == 20 or a[i] == 25 or a[i] == 30 or a[i] == 35 or a[i] == 40 or a[i] == 45:
+        f.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 6 or a[i] == 12 or a[i] == 18 or a[i] == 24 or a[i] == 30 or a[i] == 36 or a[i] == 42 or a[i] == 48 or a[i] == 54:
+        g.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 7 or a[i] == 14 or a[i] == 21 or a[i] == 28 or a[i] == 35 or a[i] == 42 or a[i] == 49 or a[i] == 56 or a[i] == 63:
+        h.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 8 or a[i] == 16 or a[i] == 24 or a[i] == 32 or a[i] == 40 or a[i] == 48 or a[i] == 56 or a[i] == 64 or a[i] == 72:
+        j.append(a[i])
+    else:
+        1 == 1
+for i in range(0, 81, 1):
+    if a[i] == 9 or a[i] == 18 or a[i] == 27 or a[i] == 36 or a[i] == 45 or a[i] == 54 or a[i] == 63 or a[i] == 72 or a[i] == 81:
+        k.append(a[i])
+    else:
+        1 == 1
+print(b[l], b[l + 1], b[l + 2], b[l + 3], b[l + 4], b[l + 5], b[l + 6], b[l + 7], b[l + 8])
+print(c[l], c[l + 1], c[l + 2], c[l + 3], c[l + 4], c[l + 5], c[l + 6], c[l + 7], c[l + 8])
+print(d[l], d[l + 1], d[l + 2], d[l + 3], d[l + 4], d[l + 5], d[l + 6], d[l + 7], d[l + 8])
+print(e[l], e[l + 1], e[l + 2], e[l + 3], e[l + 4], e[l + 5], e[l + 6], e[l + 7], e[l + 8])
+print(f[l], f[l + 1], f[l + 2], f[l + 3], f[l + 4], f[l + 5], f[l + 6], f[l + 7], f[l + 8])
+print(g[l], g[l + 1], g[l + 2], g[l + 3], g[l + 4], g[l + 5], g[l + 6], g[l + 7], g[l + 8])
+print(h[l], h[l + 1], h[l + 2], h[l + 3], h[l + 4], h[l + 5], h[l + 6], h[l + 7], h[l + 8])
+print(j[l], j[l + 1], j[l + 2], j[l + 3], j[l + 4], j[l + 5], j[l + 6], j[l + 7], j[l + 8])
+print(k[l], k[l + 1], k[l + 2], k[l + 3], k[l + 4], k[l + 5], k[l + 6], k[l + 7], k[l + 8])
+
+#e = 1
+#while e < 10:
+#    print(e, 2*e, 3*e, 4*e, 5*e, 6*e, 7*e, 8*e, 9*e)
+#    e += 1
