5c368ad8235ba495e7f1a76774089f890ade4405
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-08-13 23:38:44 +0200

    lab_7_task_2.py

--- lab_7_task_2.py
+++ lab_7_task_2.py
@@ -5,21 +5,20 @@ from matplotlib.animation import FuncAnimation
 fig, ax = plt.subplots()
  
 anim_object, = plt.plot([], [], '-', lw=2) 
-xdata, ydata = [0], [0] 
  
-ax.set_xlim(-10*2*np.pi, 10*2*np.pi) 
-ax.set_ylim(-10*2*np.pi, 10*2*np.pi) 
- 
-def update(frame): 
-    a = np.linspace(0, 2*np.pi, 1000)
-    xdata = 10 * np.cos(a) * frame  
-    ydata =  10 * np.sin(a) * frame 
+def update(R): 
+    a = np.linspace(0, 2*np.pi, 100)
+    xdata = np.cos(a) * R  
+    ydata = np.sin(a) * R 
     anim_object.set_data(xdata, ydata)
-    return anim_object,
- 
+
+plt.axis('equal')
+ax.set_xlim(-100, 100) 
+ax.set_ylim(-100, 100) 
+
 ani = FuncAnimation(fig, 
                     update, 
-                    frames=np.linspace(0, 2*np.pi, 1000),
+                    frames=150,
                     interval=20,
                     )
  
