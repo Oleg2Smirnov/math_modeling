7ce6b51e406ea54ee621f45bc42dbe729527ffc5
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-08-16 20:14:00 +0200

    lab_7_extra_task_4.py

--- lab_7_extra_task_4.py
+++ lab_7_extra_task_4.py
@@ -2,35 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-a = 10
-
 fig, ax = plt.subplots()
- 
-anim_object, = plt.plot([], [], '-', lw=2)
- 
-ax.set_xlim(-a, a)
-ax.set_ylim(-a, a)
- 
-def square_move(alpha):
-    x = [0, 1, 2, 3, 4]
-    y = [0, 1, 2, 3, 4]
-    R = (2*a**2)**0.5 / 2
-    for i in range(0, 4, 1):
-        x[i] = R*np.cos((0.25+i/2)*np.pi + alpha)
-        y[i] = R*np.sin((0.25+i/2)*np.pi + alpha)
-    x[4] = R*np.cos(0.25*np.pi + alpha)
-    y[4] = R*np.sin(0.25*np.pi + alpha)
 
-    xdata = x
-    ydata = y
-    
-    anim_object.set_data(xdata, ydata)
+anim_object, = plt.plot([], [], '-', lw=2, color='b', label='square')
+
+plt.axis('equal')
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+
+def square(t):
+    a = np.pi
+    x = [5*np.cos(t), 5*np.cos(t+0.5*a), 5*np.cos(t+a), 5*np.cos(t+1.5*a), 5*np.cos(t)]
+    y = [5*np.sin(t), 5*np.sin(t+0.5*a), 5*np.sin(t+a), 5*np.sin(t+1.5*a), 5*np.sin(t)]
+
+    anim_object.set_data(x, y)
     return anim_object,
 
 ani = FuncAnimation(fig,
-                    square_move,
-                    frames= np.arange(0, 2*np.pi, 0.1),
-                    interval=35
-                    )
- 
-ani.save('square_move.gif')
+                    square,
+                    frames=np.arange(0, 2*np.pi, 0.05),
+                    interval=20)
+
+ani.save('square.gif')
