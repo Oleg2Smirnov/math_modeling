f6d8249f453080a47fcff2847df1d64702f27d10
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2025-02-13 23:47:06 +0000

    223

--- ellips.py
+++ ellips.py
@@ -6,7 +6,7 @@ G = 6.67*10**(-11)                      # грав конст
 T1 = 1*365.26*86400                     # период обращения тела
 A1 = (G*M*T1**2/4/np.pi**2)**(1/3)      # большая полуось тела
 e = 0.8                                  # эксцентриситет тела
-alpha = 160*np.pi/180                     # угол между начальными положениями тела и Земли (тело в перицентре)
+alpha = 180*np.pi/180                     # угол между начальными положениями тела и Земли (тело в перицентре)
 A2 = 1.496*10**11                       # большая полуось Земли
 T2 = 365.26*86400                       # период обращения Земли
 
@@ -16,7 +16,7 @@ fig, ax = plt.subplots()
 
 def ellips_plotter(A1, T1, M, e, alpha, A2, T2):
     p1 = A1*(1-e**2)                                        # фокальный параметр тела
-    E = np.linspace(0, 2*np.pi, 100000)                     # эксцентрическая аномалия тела
+    E = np.linspace(0, 2*np.pi, 1000)                     # эксцентрическая аномалия тела
     t = (E-e*np.sin(E))*T1/(2*np.pi)                        # время с перицентра 
     phi_1 = 2*np.arctan(np.tan(E/2)*((1+e)/(1-e))**0.5)     # истинная аномалия тела 
     phi_2 = 2*np.pi*(t/T2)                                  # истинная аномалия Земли
@@ -26,23 +26,28 @@ def ellips_plotter(A1, T1, M, e, alpha, A2, T2):
     V1 = (G*M*(2/r1-1/A1))**0.5                             # скорость тела
     V2 = (G*M/A2)**0.5                                      # скорость Земли
     theta = alpha + phi_1 - phi_2                           # разница экл долгот 
+    #print(theta)
     L = (r1**2 + A2**2 - 2*r1*A2*np.cos(theta))**0.5        # расстояние между телом и Землёй
-    psi = np.arcsin(np.sin(theta)*r1/L)                     # фазовый угол
+    gamma = np.arcsin(np.sin(theta)*r1/L)                     # элонгационный угол
     eps = np.arccos(Vr1/V1)                                 # угол между лучевой и полной тела
+    psi = np.arcsin(np.sin(theta)*A2/L)                   # фазовый угол
+    #psi = np.arccos((r1**2 + L**2 - A2**2)/(2*r1*L))
     i = np.pi/2 + psi - eps                                 # угол между полной и трансверсальной для Земли
-    gamma = np.arcsin(np.sin(theta)*A2/L)                   # элонгационный угол
+    print(psi)
     print(i)
-    print(r1)
+    #print(r1)
     Ut = V1*np.cos(i) + V2*np.cos(gamma)                # трансверсальная относительно Земли
     Ur = (V1*np.sin(i) - V2*np.sin(gamma))/1000             # лучевая относительно Земли
-    print(Ut)
-    print(Ur)
+    #print(Ut)
+    #print(Ur)
+    #print(L)
     w = Ut/L * 180/np.pi * 86400                           # угловая относительно Земли
+    #print(w)
 
-    plt.plot(Ur, w, label='grafic')
+    plt.plot(Ur, w)#, label='')
     plt.xlabel('Ur, км/с')
     plt.ylabel('w, градус/день')
-    plt.legend()
+    #plt.legend()
     plt.savefig('ellips.png')
 	
 ellips_plotter(A1, T1, M, e, alpha, A2, T2)
--- infa.py
+++ infa.py
@@ -1,7 +1,48 @@
-print('A B C D F')
-for A in range(0, 2, 1):
-    for B in range(0, 2, 1):
-        for C in range(0, 2, 1):
-            for D in range(0, 2, 1):
-                F = (not(A) and B) or (not(C) and D)
-                print(A, B, C, D, int(F))
+import matplotlib.pyplot as plt
+import numpy as np
+
+M = 2*10**30                            # масса Солнца
+G = 6.67*10**(-11)                      # грав конст
+T1 = 1*365.26*86400                     # период обращения тела
+A1 = (G*M*T1**2/4/np.pi**2)**(1/3)      # большая полуось тела
+e = 0.8                                  # эксцентриситет тела
+alpha = 180*np.pi/180                     # угол между начальными положениями тела и Земли (тело в перицентре)
+A2 = 1.496*10**11                       # большая полуось Земли
+T2 = 365.26*86400                       # период обращения Земли
+
+fig, ax = plt.subplots()
+#ax.set_xlim(-10, 10)
+#ax.set_ylim(-0.2, 0.6)
+
+def ellips_plotter(A1, T1, M, e, alpha, A2, T2):
+    p1 = A1*(1-e**2)                                        # фокальный параметр тела
+    E = np.linspace(0, 0.5*np.pi, 100000)                     # эксцентрическая аномалия тела
+    t = (E-e*np.sin(E))*T1/(2*np.pi)                        # время с перицентра 
+    phi_1 = 2*np.arctan(np.tan(E/2)*((1+e)/(1-e))**0.5)     # истинная аномалия тела 
+    phi_2 = 2*np.pi*(t/T2)                                  # истинная аномалия Земли
+    r1 = A1*(1-e*np.cos(E))                                 # расстояние от Солнца до тела
+    Vt1 = (G*M/p1)**0.5*(1+e*np.cos(phi_1))                 # трансверсальная тела
+    Vr1 = (G*M/p1)**0.5*e*np.sin(phi_1)                     # лучевая тела
+    V1 = (G*M*(2/r1-1/A1))**0.5                             # скорость тела
+    V2 = (G*M/A2)**0.5                                      # скорость Земли
+    theta = alpha + phi_1 - phi_2                           # разница экл долгот 
+    L = (r1**2 + A2**2 - 2*r1*A2*np.cos(theta))**0.5        # расстояние между телом и Землёй
+    psi = np.arcsin(np.sin(theta)*r1/L)                     # фазовый угол
+    eps = np.arccos(Vr1/V1)                                 # угол между лучевой и полной тела
+    i = np.pi/2 + psi - eps                                 # угол между полной и трансверсальной для Земли
+    gamma = np.arcsin(np.sin(theta)*A2/L)                   # элонгационный угол
+    print(i)
+    print(r1)
+    Ut = V1*np.cos(i) + V2*np.cos(gamma)                # трансверсальная относительно Земли
+    Ur = (V1*np.sin(i) - V2*np.sin(gamma))/1000             # лучевая относительно Земли
+    print(Ut)
+    print(Ur)
+    w = Ut/L * 180/np.pi * 86400                           # угловая относительно Земли
+
+    plt.plot(Ur, w, label='grafic')
+    plt.xlabel('Ur, км/с')
+    plt.ylabel('w, градус/день')
+    plt.legend()
+    plt.savefig('ellips2.png')
+	
+ellips_plotter(A1, T1, M, e, alpha, A2, T2)
