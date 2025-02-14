0158c32381bb5680fb72f7c79dfa83334d4188dc
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2025-02-14 14:44:08 +0000

    123

--- infa.py
+++ infa.py
@@ -16,7 +16,7 @@ fig, ax = plt.subplots()
 
 def ellips_plotter(A1, T1, M, e, alpha, A2, T2):
     p1 = A1*(1-e**2)                                        # фокальный параметр тела
-    E = np.linspace(0, 0.5*np.pi, 100000)                     # эксцентрическая аномалия тела
+    E = np.linspace(0, 2*np.pi, 100000)                     # эксцентрическая аномалия тела
     t = (E-e*np.sin(E))*T1/(2*np.pi)                        # время с перицентра 
     phi_1 = 2*np.arctan(np.tan(E/2)*((1+e)/(1-e))**0.5)     # истинная аномалия тела 
     phi_2 = 2*np.pi*(t/T2)                                  # истинная аномалия Земли
@@ -29,15 +29,22 @@ def ellips_plotter(A1, T1, M, e, alpha, A2, T2):
     L = (r1**2 + A2**2 - 2*r1*A2*np.cos(theta))**0.5        # расстояние между телом и Землёй
     psi = np.arcsin(np.sin(theta)*r1/L)                     # фазовый угол
     eps = np.arccos(Vr1/V1)                                 # угол между лучевой и полной тела
-    i = np.pi/2 + psi - eps                                 # угол между полной и трансверсальной для Земли
     gamma = np.arcsin(np.sin(theta)*A2/L)                   # элонгационный угол
-    print(i)
-    print(r1)
-    Ut = V1*np.cos(i) + V2*np.cos(gamma)                # трансверсальная относительно Земли
-    Ur = (V1*np.sin(i) - V2*np.sin(gamma))/1000             # лучевая относительно Земли
-    print(Ut)
-    print(Ur)
-    w = Ut/L * 180/np.pi * 86400                           # угловая относительно Земли
+
+    u = 2*np.pi/T2 * r1
+    print('u')
+    print(u)
+    V = (V1**2 + u**2 - 2*u*V1*np.cos(np.pi/2-eps))**0.5
+    print('V')
+    print(V)
+    betta = np.arcsin(np.sin(np.pi/2-eps)*u/V)
+    print('betta')
+    print(betta)
+    g = np.pi-psi+eps-betta
+    print('g')
+    print(g)
+    Ur = -V*np.cos(g) / 1000
+    w = -V*np.sin(g)/L *180*86400/np.pi + 360*86400/T2
 
     plt.plot(Ur, w, label='grafic')
     plt.xlabel('Ur, км/с')
